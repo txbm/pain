382d9413b0c25967b79c035472cf50e6a9b4a5e8
Peter M. Elias <petermelias@gmail.com>
2013-04-29 02:28:21 -0700

    tmp dir ftl

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ from setuptools import setup, find_packages
 setup(name='pain',
       version='0.1',
       description='A scaffolding tool to mitigate the pain of setting up a new project.',
+      long_description=__doc__,
       classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Environment :: Console',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python'
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python'
       ],
       keywords='scaffolding scaffold python setup project new',
       author='Peter M. Elias',
       author_email='petermelias@gmail.com',
       url='http://github.com/petermelias/pain',
       license='FreeBSD',
+      platforms='any',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['cuisine']
+      install_requires=['cuisine'],
       test_suite = "nose.collector",
       zip_safe=False,
       entry_points = {
